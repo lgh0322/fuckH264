f6dcc43f5ac159d780ba791c017368368333924e
lgh0322 <liguanghao@viatomtech.com>
2021-09-17 15:08:32 +0800

    zvcxxc

--- app/src/main/java/com/vaca/fuckh264/MainActivity.kt
+++ app/src/main/java/com/vaca/fuckh264/MainActivity.kt
@@ -12,6 +12,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.*
+import java.nio.ByteBuffer
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var dicByteArray: ByteArray
@@ -19,9 +21,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "StudyCamera"
         private const val FRAME_RATE = 15
-        private const val REMOTE_HOST = "192.168.1.105"
-        private const val REMOTE_HOST_PORT: Short = 5000
-        private const val H264FILE = "/sdcard/test.h264"
     }
 
 
@@ -48,7 +47,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-                setupDecoder(binding.ga.holder.surface, "video/avc", 1280, 720)
+                setupDecoder(binding.ga.holder.surface, MediaFormat.MIMETYPE_VIDEO_AVC, 1280, 720)
                 dataScope.launch {
                     readH264FromFile()
                 }
@@ -67,14 +66,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupDecoder(surface: Surface?, mime: String, width: Int, height: Int): Boolean {
         Log.d(TAG, "setupDecoder surface:$surface mime:$mime w:$width h:$height")
-        val mediaFormat = MediaFormat.createVideoFormat(mime, width, height)
+        val format = MediaFormat.createVideoFormat(mime, width, height)
         mMediaDecoder = MediaCodec.createDecoderByType(mime)
+        format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 1280 *720)
+        format.setInteger(MediaFormat.KEY_MAX_HEIGHT, 720)
+        format.setInteger(MediaFormat.KEY_MAX_WIDTH, 1280)
+/*        format.setByteBuffer("csd-0", ByteBuffer.wrap(mSps))
+        format.setByteBuffer("csd-1", ByteBuffer.wrap(mPps))*/
         if (mMediaDecoder == null) {
             Log.e("DecodeActivity", "createDecoderByType fail!")
             return false
         }
 
-        mMediaDecoder!!.configure(mediaFormat, surface, null, 0)
+        mMediaDecoder!!.configure(format, surface, null, 0)
         mMediaDecoder!!.start()
         return true
     }
@@ -84,12 +88,29 @@ class MainActivity : AppCompatActivity() {
     private fun readH264FromFile() {
         try {
             val dictionaryStream: InputStream = assets.open("w.h264")
-            var len = 0
-            val fis = dictionaryStream
-            val buf = ByteArray(1024)
-            while (fis.read(buf).also { len = it } > 0) {
-                offerDecoder(buf, len)
+            var len = dictionaryStream.available()
+            val fileBytes=ByteArray(len)
+            dictionaryStream.read(fileBytes)
+
+
+            start@ for (i in 0 until fileBytes.size) {
+                if (fileBytes[i] == 0.toByte() && fileBytes[i + 1] == 0.toByte() && fileBytes[i + 2] == 0.toByte() && fileBytes[i + 3] == 1.toByte()
+                ) {
+                    end@ for (j in i + 4 until fileBytes.size) {
+                        if (fileBytes[j] == 0.toByte() && fileBytes[j + 1] == 0.toByte() && fileBytes[j + 2] == 0.toByte() && fileBytes[j + 3] == 1.toByte()
+                        ) {
+                            val temp: ByteArray = Arrays.copyOfRange(
+                                fileBytes, i, j
+                            )
+                            offerDecoder(temp,temp.size)
+                            break@end
+                        }
+                    }
+                }
             }
+
+
+
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
         } catch (e: IOException) {
@@ -104,11 +125,10 @@ class MainActivity : AppCompatActivity() {
             val inputBufferIndex = mMediaDecoder!!.dequeueInputBuffer(-1)
             if (inputBufferIndex >= 0) {
                 val inputBuffer = inputBuffers[inputBufferIndex]
-                val timestamp = (mFrameIndex++ * 1000000 / FRAME_RATE).toLong()
-                Log.d(TAG, "offerDecoder timestamp: $timestamp inputSize: $length bytes")
-                inputBuffer.clear()
                 inputBuffer.put(input, 0, length)
-                mMediaDecoder!!.queueInputBuffer(inputBufferIndex, 0, length, timestamp, 0)
+                inputBuffer.clear()
+                inputBuffer.limit(length)
+                mMediaDecoder!!.queueInputBuffer(inputBufferIndex, 0, length, 0, MediaCodec.BUFFER_FLAG_SYNC_FRAME)
             }
             val bufferInfo = MediaCodec.BufferInfo()
             var outputBufferIndex = mMediaDecoder!!.dequeueOutputBuffer(bufferInfo, 0)
