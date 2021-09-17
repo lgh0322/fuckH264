8c43008a022fb05282e74cc152401e29bed669e3
lgh0322 <liguanghao@viatomtech.com>
2021-09-17 18:00:46 +0800

    fgjtetyuhdgfhsgdasfgdghdthdhyhhgjfxg

--- app/src/main/java/com/vaca/fuckh264/MainActivity.kt
+++ app/src/main/java/com/vaca/fuckh264/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.vaca.fuckh264
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
+import android.media.ImageReader
 import android.media.MediaCodec
 import android.media.MediaFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
+import android.util.Size
 import android.view.Surface
 import android.view.SurfaceHolder
+import android.view.TextureView
 import com.vaca.fuckh264.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -20,15 +30,150 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
 
+
+
+
+
+    private val mCameraId = "0"
+    lateinit var mPreviewSize: Size
+    private val PREVIEW_WIDTH = 1920
+    private val PREVIEW_HEIGHT = 1080
+    private var mCameraDevice: CameraDevice? = null
+    lateinit var mHandler: Handler
+    lateinit var mCaptureSession: CameraCaptureSession
+    lateinit var mPreviewBuilder: CaptureRequest.Builder
+    private var mHandlerThread: HandlerThread? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.ga.surfaceTextureListener=object: TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
 
+            }
 
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+              startBackgroundThread()
+                openCamera()
 
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+               return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+
+            }
+
+        }
     }
 
 
+
+
+    private fun startBackgroundThread() {
+        mHandlerThread = HandlerThread("fuck")
+        mHandlerThread!!.start()
+        mHandler = Handler(mHandlerThread!!.looper)
+    }
+
+
+    private val mCameraDeviceStateCallback: CameraDevice.StateCallback =
+        object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                mCameraDevice = camera
+                startPreview(camera)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                Log.e("fuckCamera", "a1")
+                camera.close()
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                Log.e("fuckCamera", "a2")
+                camera.close()
+            }
+
+            override fun onClosed(camera: CameraDevice) {
+                Log.e("fuckCamera", "a3")
+                camera.close()
+            }
+        }
+
+    private fun openCamera() {
+        try {
+            val cameraManager =
+                getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            mPreviewSize = Size(PREVIEW_WIDTH, PREVIEW_HEIGHT)
+            cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, mHandler)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+
+    private val mSessionStateCallback: CameraCaptureSession.StateCallback =
+        object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                mCaptureSession = session
+                updatePreview()
+
+            }
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {}
+        }
+
+    private fun startPreview(camera: CameraDevice) {
+        mPreviewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_RECORD)
+        val fuck=binding.ga.surfaceTexture
+        mPreviewBuilder.addTarget(mImageReader.surface)
+
+
+        camera.createCaptureSession(
+            Arrays.asList(mImageReader.surface),
+            mSessionStateCallback,
+            mHandler
+        )
+    }
+
+    private fun updatePreview() {
+        mHandler.post(Runnable {
+            try {
+                mCaptureSession.setRepeatingRequest(mPreviewBuilder.build(), null, mHandler)
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+        })
+    }
+
+    override fun onPause() {
+        closeCamera()
+        super.onPause()
+
+    }
+
+    private fun closeCamera() {
+        mCaptureSession.stopRepeating()
+        mCaptureSession.close()
+        mCameraDevice!!.close()
+        stopBackgroundThread()
+    }
+
+    private fun stopBackgroundThread() {
+        try {
+            if (mHandlerThread != null) {
+                mHandlerThread!!.quitSafely()
+                mHandlerThread!!.join()
+                mHandlerThread = null
+            }
+            mHandler.removeCallbacksAndMessages(null)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
 
 }
